8dd728b8a16d7e26b189624f20fa8ea75d72e7a8
hieu2642 <ngoduchieu2004@gamil.com>
2023-08-06 18:13:47 +0700

    hk

--- ontP.c
+++ ontP.c
@@ -2,10 +2,10 @@
 
 int main(){
     int f = 7;
-int k =9;
+
     int a = 5;
     int b = 2;
-    float c = (float)a / (float)b;
+    float c = (float)a / (float)b  + 1;
     printf("%f",c);
     printf("m");
     printf("gg");
